2026f84f250b949959a246bd7e45094f67268131
Anas-Iqbal-77 <iqbalanas840@gmail.com>
2024-09-28 01:31:09 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,137 @@
+//question2
+//var num1 = +prompt("Enter first num")
+//var num2 = +prompt("Enter second num")
+
+//if(num1 > num2){
+  //  document.write("its a bigger number" + num1 )
+//}
+//else if( num2 > num1)
+//{ 
+ //   document.write("its biggerr number" + num2)
+//}
+
+//else if(num1 === num2){
+  //  document.write("its equal numbers")
+//}
+
+//question3
+
+//let number = +prompt("enter a number");
+
+//if(number > 0){
+ //   document.write("its a positive number")
+//}
+
+//else if(number <  0){
+  //  document.write("its a negative number")
+  
+//}
+
+//else if(number == 0){
+  //  document.write("its a zero")
+//}
+
+
+//question4
+
+//let letter = prompt("enter a character")
+
+//if (letter == "a"){
+  //  document.write("its a vowel")
+//}
+
+//else if( letter== "e"){
+  //      document.write("its avowel")
+//}
+
+//else if(letter== "i"){
+  //  document.write("its avowel")
+//}
+//else if( letter == "o"){
+  //  document.write("its a vowel")
+//}
+
+//else if( letter == "u"){
+  //  document.write( "its avowel")
+//}
+
+//else{
+  //  document.write("its not a vowel")
+//}
+
+//question5
+//let registeredPassword = prompt("Register your password:");
+
+
+//if (registeredPassword) {
+  //document.write("Registered password successfully" + "<br>");
+//} 
+//else {
+  //document.write("Please register your password" + "<br>");
+//}
+
+//let userpassword = prompt("enter your registered password");
+
+//if(userpassword == registeredPassword){
+  //document.write("log in successfully")
+//}
+
+//else{
+  //document.write("incorrect password")
+//}
+
+//question6
+//var greeting;
+//var hour = 13;
+//if (hour < 18) {
+//greeting = "Good day";
+//}
+//else{
+//greeting = "Good evening";
+//}
+
+//question7
+
+
+
+//let time = +prompt("enter time")
+
+//if(time == 00 && time <= 1159){
+  //document.write("good morning")
+//}
+
+//else if(time >= 1200 && time <=1759 ){
+  //document.write("good afternoon")
+//}
+
+//else if(time >= 1800 && time <= 2000){
+  //document.write("good evening")
+//}
+
+//else if(time >= 2100 && time <= 2300){
+  //document.write("good night")
+//}
+
+
+//question1
+//let character = prompt("Enter a character");
+//let asciiCode = character.charCodeAt(0);
+//if (asciiCode > 29 && asciiCode < 40){
+//alert("Given input  is a number")
+//} 
+//else if (asciiCode > 64 && asciiCode < 91){
+//alert("Given input is uppercase letter")
+//}
+//else if (asciiCode > 96 && asciiCode < 123){
+//alert("Given input is lowercase letter")
+//}
+
+
+ 
+
+
+
+
+
+
+
